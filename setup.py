032bc215e9c661d0415328c8ec5c0d64b6e80b75
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2024-11-24 11:42:41 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="petkitaio",
     version="0.1.11.3",
-    author="Robert Drinovac",
-    author_email="unlisted@gmail.com",
+    author="Jezza34000",
+    author_email="mail@me.org",
     description="Asynchronous Python library for PetKit's API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/RobertD502/petkitaio',
-    keywords='petkit, eversweet 3 pro, feeder mini, d4, petkit feeder, petkit water fountain, freshelement solo, pura x, pura max, pura air',
+    url="https://github.com/Jezza34000/petkitaio",
+    keywords="petkit, eversweet 3 pro, feeder mini, d4, petkit feeder, petkit water fountain, freshelement solo, pura x, pura max, pura air, purobot, eversweet max, pura max 2",
     packages=setuptools.find_packages(),
-    python_requires= ">=3.7",
+    python_requires=">=3.11",
     install_requires=[
         "aiohttp>=3.8.1",
         "tzlocal>=4.2",
@@ -22,10 +22,10 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
+        "Operating System :: OS Independent",
     ],
     project_urls={  # Optional
-    'Bug Reports': 'https://github.com/RobertD502/petkitaio/issues',
-    'Source': 'https://github.com/RobertD502/petkitaio/',
+        "Bug Reports": "https://github.com/Jezza34000/petkitaio/issues",
+        "Source": "https://github.com/Jezza34000/petkitaio",
     },
 )
