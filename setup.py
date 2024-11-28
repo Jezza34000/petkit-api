d07a9b74685f4da5f625b9077b0509b81c00a1fd
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2024-11-28 17:32:57 +0100

    update information

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Jezza34000/petkit-api",
-    keywords="petkit, eversweet 3 pro, feeder mini, d4, petkit feeder, petkit water fountain, freshelement solo, pura x, pura max, pura air, purobot, eversweet max, pura max 2",
+    keywords="petkit, eversweet 3 pro, feeder mini, d4, petkit feeder, petkit water fountain, freshelement solo, pura x, pura max, pura air, purobot, eversweet max, pura max 2, yumshare dual hopper",
     packages=setuptools.find_packages(),
     python_requires=">=3.11",
     install_requires=[
